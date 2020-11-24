c21f7e71bd3d3153962b922bfe751693f9d2b19d
Rafael <akafael@users.noreply.github.com>
2020-11-23 23:42:24 -0300

    Add code for identification

---
+++ src/arduino/test_dcmotor_pulses/test_dcmotor_pulses.ino
@@ -0,0 +1,112 @@
+#include <elapsedMillis.h>
+
+#define PINLED 13
+#define PINMOTOR1 6
+#define PINMOTOR2 5
+#define PINMOTOR_EN 9
+
+#define PINENCODER_A 2
+#define PINENCODER_B 3
+
+#define DEADZONE 150
+
+elapsedMillis timerGlobal;
+elapsedMillis timerLoop;
+elapsedMillis timerMotorOn;
+const int timeStep = 100; // ms
+int timeMotorOn = 10*timeStep; // ms
+
+bool isMotorON = false;
+uint8_t speed = DEADZONE;
+uint8_t stepSpeed = 5;
+uint8_t counter = 0;
+
+// Encoder
+volatile long  encoderCount = 0;
+volatile bool isRotatingCCW = true;
+long  encoderCountTotal = 0;
+
+void setup() {
+  
+  Serial.begin(9600);
+
+  pinMode(PINLED, OUTPUT);
+  pinMode(PINMOTOR1, OUTPUT);
+  pinMode(PINMOTOR2, OUTPUT);
+  pinMode(PINMOTOR_EN, OUTPUT);
+
+  digitalWrite(PINMOTOR_EN,HIGH);
+  analogWrite(PINMOTOR1,0);
+  analogWrite(PINMOTOR2,0);
+
+  // Encoder - Set PIN
+  pinMode(PINENCODER_A, INPUT_PULLUP);
+  pinMode(PINENCODER_B, INPUT_PULLUP);
+
+  // Attach Interrupt signal to Encoder PIN
+  attachInterrupt(digitalPinToInterrupt(PINENCODER_A), isrCount, RISING); 
+
+  timerMotorOn = 0;
+  timerGlobal = 0;
+}
+
+void loop() {
+  if( timerLoop >= timeStep ) // Control Frequency time
+  {
+    // Reset Timer
+    timerLoop = 0; 
+
+    // Read Encoder
+    noInterrupts();
+    encoderCountTotal += encoderCount;
+    encoderCount = 0;
+    interrupts();
+
+    if (timerMotorOn >= timeMotorOn )
+    {
+      timerMotorOn = 0;
+      isMotorON = !isMotorON;
+      counter = (counter + 1) & 15;
+      speed = counter*stepSpeed + DEADZONE;
+      timeMotorOn = (20-counter)*timeStep;
+    }
+
+    if ( isMotorON )
+    {
+      // Set Motor Speed
+      digitalWrite(PINMOTOR2,LOW);
+      analogWrite(PINMOTOR1,speed);
+    }
+    else
+    {
+      digitalWrite(PINMOTOR1,LOW);
+      digitalWrite(PINMOTOR2,LOW);
+    }
+
+    // Output
+    Serial.print(timerGlobal);
+    Serial.print(",");
+    Serial.print(encoderCountTotal);
+    Serial.print(",");
+    Serial.println( speed * isMotorON );
+  }
+}
+
+
+/**
+ * Count Enconder Steps
+ *  - Called by Interruption
+ */
+void isrCount()
+{
+  // Detect Rotation Direction
+  isRotatingCCW = digitalRead(PINENCODER_B);
+  if(isRotatingCCW == HIGH)
+  {
+    encoderCount--;
+  }
+  else
+  {
+    encoderCount++;
+  }
+}
